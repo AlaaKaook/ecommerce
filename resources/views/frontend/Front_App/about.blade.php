0cc8c5a120ac8c18ae24e314cc0d27c70d6946c5
AlaaKaook <alaaqaouq1998@gmail.com>
2022-11-19 03:34:04 +0300

    about section in FrontEnd

---
+++ resources/views/frontend/Front_App/about.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.front_end2.layaout_front')
+
+@section('content')
+    <!-- Product Details Section Begin -->
+    <section class="product-details spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6">
+                    <div class="product__details__pic">
+                        <div class="product__details__pic__item">
+                            <img class="product__details__pic__item--large"
+                                src="{{ asset('assets\frontend2\img\about.jfif') }}" alt="Image Product" width="555px"
+                                height="575px">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="about__text">
+                        <h3>ALAA SHOP</h3>
+                        <hr>
+                        Sed porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit, eget
+                        tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                        Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl
+                        tempus convallis quis ac lectus. Proin eget tortor risus. Donec rutrum congue leo eget malesuada.
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec sollicitudin molestie
+                        malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        Sed porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit, eget
+                        tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                        Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. Curabitur non nulla sit amet nisl
+                        tempus convallis quis ac lectus. Proin eget tortor risus. Donec rutrum congue leo eget malesuada.
+                        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Donec sollicitudin molestie
+                        malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.
+                        <hr>
+                        <div class="about__text">
+                            <h4>ALAA KAOOK 2022</h4>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- Product Details Section End -->
+@endsection
